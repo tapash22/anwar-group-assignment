60d961094895f638e8bcae234cfcb745a192cd06
tapash22 <tapasp263@gmail.com>
2024-01-09 19:08:30 +0600

    timeline dot make customize

--- src/components/TimeLIne.vue
+++ src/components/TimeLIne.vue
@@ -1,76 +1,59 @@
 <template>
-<div class="d-block">
+  <div class="d-block">
     <div class="d-flex justify-center py-2">
-        <v-btn
-      depressed
-      color="primary"
-      @click="openForm"
-    >
-    Add New post
-    </v-btn>
+      <v-btn depressed color="primary" @click="openForm"> Add New post </v-btn>
     </div>
+
     <div class="d-flex justify-center py-2">
-        <p class="heading">
-           Our Company Milestone
-        </p>
+      <p class="heading">Our Company Milestone</p>
     </div>
-    <v-timeline reverse="reverse" >
-    <v-timeline-item v-for="(item, index) in userDetails" :key="index" small>
-      <v-card>
-        <v-row>
-          <!-- Left side -->
+    <v-timeline reverse="reverse">
+      <v-timeline-item v-for="(item, index) in userDetails" :key="index"  >
+        <v-card>
+          <v-row>
+            <!-- Left side -->
+            <v-col :cols="10">
+              <FirstCardVue v-if="item.image" :item="item" />
+              <SecondCardVue v-else-if="item.images" :item="item" />
+              <ThirdCardVue v-else :item="item" />
+            </v-col>
+            <!-- Left side end -->
 
-          <v-col :cols="10">
-            <FirstCardVue v-if="item.image" :item="item" />
-            <SecondCardVue v-else-if="item.images" :item="item" />
-            <ThirdCardVue v-else :item="item" />
-          </v-col>
+            <!-- Right side -->
+            <v-col
+              class="text-center border-left pa-0 ma-0 d-flex align-center justify-center pt-auto pb-auto"
+              :cols="2"
+            >
+              <DataFormat :item="item" />
+            </v-col>
 
-          <!-- Right side -->
+             <!-- Right side end -->
+          </v-row>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
 
-          <v-col
-            class="text-center border-left pa-0 ma-0 d-flex align-center justify-center pt-auto pb-auto"
-            :cols="2"
-          >
-            <div>
-              <v-card-subtitle class="py-0 sub-title">{{
-                formatMonth(item.date)
-              }}</v-card-subtitle>
-              <v-card-subtitle class="py-0 title">{{
-                formatDay(item.date)
-              }}</v-card-subtitle>
-              <v-card-text class="py-0 sub-title">{{
-                formatYear(item.date)
-              }}</v-card-text>
-            </div>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-timeline-item>
-  </v-timeline>
-
-  <div v-if="show" class="form-container" >
-    <UserForm @add-new-data="addNewData" />
+    <div v-if="show" class="form-container">
+      <UserForm @add-new-data="addNewData" />
+    </div>
   </div>
-</div>
 </template>
   
   <script>
 import FirstCardVue from "./card/FirstCard.vue";
 import SecondCardVue from "./card/SecondCard.vue";
 import ThirdCardVue from "./card/ThirdCard.vue";
-import UserForm from './form/UserForm.vue';
-
+import UserForm from "./form/UserForm.vue";
+import DataFormat from "./card/DateFormat.vue";
 
 export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
 
-    data(){
-        return{
-            show:false,
-        }
-    },
-
-    computed: {
+  computed: {
     userDetails() {
       return this.$store.getters.getUserDetails;
     },
@@ -80,34 +63,27 @@ export default {
     FirstCardVue,
     SecondCardVue,
     ThirdCardVue,
-    UserForm
+    UserForm,
+    DataFormat,
   },
 
   methods: {
-    openForm(){
-        this.show = true;
+    openForm() {
+      this.show = true;
     },
 
     addNewData(newDetail) {
-        console.log(newDetail);
-      this.$store.dispatch('addUserDetail', newDetail);
+      console.log(newDetail);
+      this.$store.dispatch("addUserDetail", newDetail);
       this.show = false;
-    },
-
-    formatMonth(dateString) {
-      const date = new Date(dateString);
-      return date.toLocaleDateString(undefined, { month: "short" });
-    },
-    formatDay(dateString) {
-      const date = new Date(dateString);
-      return date.toLocaleDateString(undefined, { day: "numeric" });
-    },
-    formatYear(dateString) {
-      const date = new Date(dateString);
-      return date.toLocaleDateString(undefined, { year: "numeric" });
     },
   },
 };
 </script>
+
+<style >
+
+
+</style>
 
   
---
+++ src/components/card/DateFormat.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-card-subtitle class="py-0 sub-title">{{
+      formatMonth(item.date)
+    }}</v-card-subtitle>
+    <v-card-subtitle class="py-0 title">{{
+      formatDay(item.date)
+    }}</v-card-subtitle>
+    <v-card-text class="py-0 sub-title">{{
+      formatYear(item.date)
+    }}</v-card-text>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+
+  methods: {
+    formatMonth(dateString) {
+      const date = new Date(dateString);
+      return date.toLocaleDateString(undefined, { month: "short" });
+    },
+    formatDay(dateString) {
+      const date = new Date(dateString);
+      return date.toLocaleDateString(undefined, { day: "numeric" });
+    },
+    formatYear(dateString) {
+      const date = new Date(dateString);
+      return date.toLocaleDateString(undefined, { year: "numeric" });
+    },
+  },
+};
+</script>
